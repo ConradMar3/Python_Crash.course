e1dba47a8ed0e39ec2142f2d806767ebb18f17c9
ConradMare890317 <Conradmare@gmail.com>
2021-02-24 15:46:36 +0200

    2021/02/24 (pm[b])

--- Data_Visualization(Project_2)/Defining_colors.py
+++ Data_Visualization(Project_2)/Defining_colors.py
@@ -7,7 +7,12 @@ y_values = [x**2 for x in x_values]
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 
-ax.scatter(x_values, y_values, s=10)
+#Defining custom colors ( add 'c=?' to scatter())
+#'C=(0,0,0)' is the RGB model.
+#ax.scatter(x_values, y_values, c='violet', s=10)
+
+#Using a colormap
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 #Set chart title and label axes
 ax.set_title("Square Numbers", fontsize=24)
@@ -17,4 +22,9 @@ ax.set_ylabel("Square of Value", fontsize=14)
 #Set the range of each axis
 ax.axis([0, 1100, 0, 1100000])
 
-plt.show()
+#plt.show()
+
+#Saving plot automatically: use 'plt.savefig()'
+plt.savefig('squares_plot.png', bbox_inches='tight')
+
+#Note: 'plt.savefig()' creates a .png file.
