d8728bdc821bf6055a05d0278e005893beddbcca
ConradMare890317 <Conradmare@gmail.com>
2021-02-26 16:41:44 +0200

    2021/02/26 (pm)

---
+++ Rolling_Dice/die_visual.py
@@ -0,0 +1,26 @@
+#from die import Die
+
+#Create a D6.
+#die = Die()
+
+#Roll, and store results in a list.
+#results = []
+
+#for roll_num in range(100):
+	#result = die.roll()
+	#results.append(result)
+
+#print(results)
+
+#Analyzing the results.
+#for roll_num in range(1000):
+	#result = die.roll()
+	#results.append(result)
+
+#frequencies = []
+
+#for value in range(1, die.num_sides+1):
+	#frequency = results.count(value)
+	#frequencies.append(frequency)
+
+#print(frequencies)
