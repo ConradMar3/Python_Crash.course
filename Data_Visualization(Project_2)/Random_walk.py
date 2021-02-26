b2a51da8985fe3747bbeaaea636801786d86ca5a
ConradMare890317 <Conradmare@gmail.com>
2021-02-26 13:53:12 +0200

    2021/02/26 (pm)

--- Data_Visualization(Project_2)/Random_walk.py
+++ Data_Visualization(Project_2)/Random_walk.py
@@ -3,7 +3,7 @@ from random import choice
 class RandomWalk:
 	"""A class to generate random walks."""
 
-	def __init__(self, num_points=50_000):
+	def __init__(self, num_points=250_000):
 		"""Initialize attributes of a walk."""
 		self.num_points = num_points
 
--- Data_Visualization(Project_2)/rw_visual.py
+++ Data_Visualization(Project_2)/rw_visual.py
@@ -4,12 +4,13 @@ from Random_walk import RandomWalk
 #Keep making new walks, as long as the program is active.
 while True:
 	#Make a random walk.
-	rw = RandomWalk(50_000)
+	rw = RandomWalk(250_000)
 	rw.fill_walk()
 
 	#Plot the points in the walk.
 	plt.style.use('classic')
-	fig, ax = plt.subplots(figsize=(15, 9))
+	fig, ax = plt.subplots(figsize=(20, 10))
+	#fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
 	
 
 	#Coloring the points
@@ -21,9 +22,9 @@ while True:
 	ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Purples,
 		edgecolors='none', s=1)
 	# Emphasize the first and last points.
-	ax.scatter(0, 0, c='orange', edgecolors='none', s=100)
+	ax.scatter(0, 0, c='orange', edgecolors='none', s=50)
 	ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
-		s=100)
+		s=50)
 
 	# Remove the axes.
 	ax.get_xaxis().set_visible(False)
