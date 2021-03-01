4060d29862d52d10cad8f4ca389a37d2728b1c42
ConradMare890317 <Conradmare@gmail.com>
2021-03-01 18:39:09 +0200

    2021/03/01 (pm)

--- Matplotlib_Advanced_Python/3D_plot (Multiple_values).py
+++ Matplotlib_Advanced_Python/3D_plot (Multiple_values).py
@@ -4,15 +4,15 @@ from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-x1 = np.random.rand(10)
-y1 = np.random.rand(10)
-z1 = np.random.rand(10)
+x1 = np.random.rand(5)
+y1 = np.random.rand(5)
+z1 = np.random.rand(5)
 
-x2 = np.random.rand(10)
-y2 = np.random.rand(10)
-z2 = np.random.rand(10)
+x2 = np.random.rand(5)
+y2 = np.random.rand(5)
+z2 = np.random.rand(5)
 
-#ax.scatter(x1,y1,z1,s=20, color='red', marker='^')
+ax.scatter(x1,y1,z1,s=20, color='red', marker='^')
 ax.scatter(x2,y2,z2,s=20, color='blue', marker='o')
 ax.plot(x1, y1, z1, color='green')
 
