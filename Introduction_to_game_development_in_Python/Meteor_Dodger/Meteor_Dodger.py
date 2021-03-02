0f1876182da1206e7979f9a8af37f97719db67b0
ConradMare890317 <Conradmare@gmail.com>
2021-03-02 20:28:42 +0200

    2021/03/02 (pm)

--- Introduction_to_game_development_in_Python/Meteor_Dodger/Meteor_Dodger.py
+++ Introduction_to_game_development_in_Python/Meteor_Dodger/Meteor_Dodger.py
@@ -5,16 +5,27 @@ class SpaceShip(pygame.sprite.Sprite):
 		super().__init__()
 		self.image = pygame.image.load(path)
 		self.rect = self.image.get_rect(center = (x_pos, y_pos))
+		self.shield_surface = pygame.image.load('shield.png')
+		self.health = 5
 
 	def update(self):
 		self.rect.center = pygame.mouse.get_pos()
 		self.screen_constrain()
+		self.display_health()
 
 	def screen_constrain(self):
 		if self.rect.right >= 1280:
 			self.rect.right = 1280
 		if self.rect.left <= 0:
 			self.rect.left = 0
+
+	def display_health(self):
+		for index, shield in enumerate(range(self.health)):
+			screen.blit(self.shield_surface,(10 + index * 40, 10))
+
+	def get_damage(self, damage_amount):
+		self.health -= damage_amount
+
 
 class Meteor(pygame.sprite.Sprite):
 	def __init__(self, path, x_pos, y_pos,x_speed, y_speed):
@@ -32,9 +43,38 @@ class Meteor(pygame.sprite.Sprite):
 			self.kill()
 
 
+class Laser(pygame.sprite.Sprite):
+	def __init__(self, path, pos, speed):
+		super().__init__()
+		self.image = pygame.image.load(path)
+		self.rect = self.image.get_rect(center = pos)
+		self.speed = speed
+
+	def update(self):
+		self.rect.centery -= self.speed
+		if self.rect.centery <= 100:
+			self.kill()
+
+def main_game():
+	laser_group.draw(screen)
+	spaceship_group.draw(screen)
+	meteor_group.draw(screen)
+	
+	laser_group.update()
+	spaceship_group.update()
+	meteor_group.update()
+
+def end_game():
+	text_surface = game_font.render('Game Over', True, (255, 255, 255))
+	text_rect = text_surface.get_rect(center = (640, 360))
+	screen.blit(text_surface, text_rect)
+
+
+
 pygame.init() #Initiate pygame
 screen = pygame.display.set_mode((1280,720)) #Create display surface
 clock = pygame.time.Clock() #Create clock object
+game_font = pygame.font.Font(None, 50)
 
 
 spaceship = SpaceShip('spaceship.png', 640, 500, 10)
@@ -48,6 +88,8 @@ meteor_group.add(meteor)
 METEOR_EVENT =pygame.USEREVENT
 pygame.time.set_timer(METEOR_EVENT, 250)
 
+laser_group = pygame.sprite.Group()
+
 while True: #Game loop
 	for event in pygame.event.get(): #Check for events / Player input
 		if event.type == pygame.QUIT: #Close the game
@@ -55,21 +97,33 @@ while True: #Game loop
 			sys.exit()
 
 		if event.type == METEOR_EVENT:
+			random_x_speed = random.randrange(-1, 1)
 			meteor_path = random.choice(('Meteor1.png', 'Meteor2.png', 'Meteor3.png'))
 			random_x_pos = random.randrange(0, 1280)
 			random_y_pos = random.randrange(-500, -50)
-			random_x_speed = random.randrange(-1, 1)
+
 			random_y_speed = random.randrange(4, 10)
 			meteor = Meteor(meteor_path, random_x_pos, random_y_pos, random_x_speed, random_y_speed)
 			meteor_group.add(meteor)
 
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			new_laser = Laser('Laser.png', event.pos, 20)
+			laser_group.add(new_laser)
 
 	screen.fill((42,45,51))
 
-	spaceship_group.draw(screen)
-	spaceship_group.update()
+		# Collision
+	if pygame.sprite.spritecollide(spaceship_group.sprite, meteor_group, True):
+		spaceship_group.sprite.get_damage(1)
 
-	meteor_group.draw(screen)
-	meteor_group.update()
+	for laser in laser_group:
+		pygame.sprite.spritecollide(laser, meteor_group, True)
+ 
+
+	if spaceship_group.sprite.health > 0:
+		main_game()
+	else:
+		end_game()
+
 	pygame.display.update() #Draw frame
 	clock.tick(120) #Control frame rate
